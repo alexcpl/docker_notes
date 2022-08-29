dd003ca4fff34e8eb16ed3e4c4c3426e25894911
Alex Cheung <alex@alexcheung.xyz>
2022-08-29 10:05:49 +0800

    Update ubuntu-server.sh

--- ubuntu-server.sh
+++ ubuntu-server.sh
@@ -22,4 +22,5 @@ $ docker run -d -p 8000:8000 -p 9443:9443 --name portainer \
 
 # Install webmin - https://webmin.com/deb.html
 $ wget http://prdownloads.sourceforge.net/webadmin/webmin_2.000_all.deb
+$ apt-get install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python unzip
 $ dpkg --install webmin_2.000_all.deb
