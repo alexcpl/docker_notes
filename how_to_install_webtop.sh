3d7b53d430ff394ded1fd52e5d3876db92453514
Alex Cheung <alex@alexcheung.xyz>
2022-09-01 23:21:17 +0800

    Update how_to_install_webtop.sh

--- how_to_install_webtop.sh
+++ how_to_install_webtop.sh
@@ -1,4 +1,25 @@
-
+# docker compose webtop - https://docs.linuxserver.io/images/docker-webtop
+---
+version: "2.1"
+services:
+  webtop:
+    image: lscr.io/linuxserver/webtop:ubuntu-mate
+    container_name: webtop
+    security_opt:
+      - seccomp:unconfined #optional
+    environment:
+      - PUID=1000
+      - PGID=1000
+      - TZ=Asia/Hong_Kong
+      - SUBFOLDER=/ #optional
+      - KEYBOARD=en-us-qwerty #optional
+    volumes:
+      - /home/alexcpl/dockers/webtop/config:/config
+      - /var/run/docker.sock:/var/run/docker.sock #optional
+    ports:
+      - 3000:3000
+    shm_size: "1gb" #optional
+    restart: unless-stopped
 
 # Display Chinese in webtop browser
 $ sudo apt update
