5c3f4c872a8ef9e7ede2342183d97f84752937ea
Alex Cheung <alex@alexcheung.xyz>
2022-09-03 07:56:50 +0800

    Update how_to_config_minecraft_server.sh

--- how_to_config_minecraft_server.sh
+++ how_to_config_minecraft_server.sh
@@ -14,4 +14,9 @@ services:
     stdin_open: true
     tty: true
     
-    # after stack run navigate to data directory and find server.properties to edit your world generate properties
+# after stack run navigate to data directory and find server.properties to edit your world generate properties
+# on portainer navigate to container page, find your minecraft server container and click on the Attach link to access your server conaole
+# in there you can add user to op role
+# more setting please visit 
+# https://nodecraft.com/support/games/minecraft-bedrock/how-to-setup-op-admin-in-your-minecraft-bedrock-edition-server#h-lesscodegreaterpermissionsjsonlesscodegreater-file-efb7208017
+# permissions.json file for user id role table
