795d7909465b3cafb9c51ddf06d9b6e145bafe3f
Alex Cheung <alex@alexcheung.xyz>
2022-09-03 08:29:18 +0800

    Update how_to_install_frps_frpc.sh

--- how_to_install_frps_frpc.sh
+++ how_to_install_frps_frpc.sh
@@ -22,3 +22,57 @@ docker run \
 -v ./frpc.ini:/etc/frp/frpc.ini \ # because where you run the script, just in case change (.) to your absolute path
 --name frpc \
 snowdreamtech/frpc
+
+# frpc.ini
+[common]
+server_addr = [YOUR SERVER IP ADDRESS]
+server_port = 7000
+
+[DSM-https]
+type = tcp
+local_ip = 192.168.1.200
+local_port = 5001
+remote_port = 5001
+custom_domains = custom.example.com
+
+[web-https]
+type = tcp
+local_ip = 192.168.1.200
+local_port = 443
+remote_port = 443
+custom_domains = frp-https.example.com
+
+[web-http]
+type = tcp
+local_ip = 192.168.1.200
+local_port = 80
+remote_port = 80
+custom_domains = frp.example.com
+
+[site2-https]
+type = tcp
+local_ip = 192.168.1.200
+local_port = 443
+remote_port = 4431
+custom_domains = frp.example.com
+
+[ssh]
+type = tcp
+local_ip = 192.168.1.200
+local_port = 22
+remote_port = 6339
+custom_domains = ssh.example.com
+
+[range:services]
+type = tcp
+local_ip = 192.168.1.201
+local_port = 21115-21119
+remote_port = 21115-21119
+custom_domains = services.example.com
+
+[services21116]
+type = udp
+local_ip = 192.168.1.201
+local_port = 21116
+remote_port = 21116
+custom_domains = services.example.com
