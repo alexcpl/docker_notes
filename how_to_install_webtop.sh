51be9113d32c48ee2484e956da793040ae5de4de
Alex Cheung <alex@alexcheung.xyz>
2022-09-03 09:19:05 +0800

    Update how_to_install_webtop.sh

--- how_to_install_webtop.sh
+++ how_to_install_webtop.sh
@@ -27,6 +27,6 @@ $ sudo apt install wget
 $ wget https://github.com/sonatype/maven-guide-zh/raw/master/content-zh/src/main/resources/fonts/simsun.ttc
 $ wget https://github.com/alexcpl/Linux_Chinese_Font_types/blob/master/simsun.ttc
 $ sudo mkdir /usr/share/fonts/win
-$ mv simsun.ttc /usr/share/fonts/win
+$ sudo mv simsun.ttc /usr/share/fonts/win
 $ fc-cache -vf
 $ fc-list :lang=zh
