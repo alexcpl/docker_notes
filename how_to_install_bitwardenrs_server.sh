0a5caa23a0e6ca5b0ee790c681a2c29b5d606648
Alex Cheung <alex@alexcheung.xyz>
2022-09-03 12:02:32 +0800

    Update how_to_install_bitwardenrs_server.sh

--- how_to_install_bitwardenrs_server.sh
+++ how_to_install_bitwardenrs_server.sh
@@ -1,23 +1,6 @@
---- This image is deprecated ---
-version: '2'
-
-services:
-  bitwarden:
-    image: bitwardenrs/server
-    restart: always
-    ports:
-      - 5555:80
-    volumes:
-      - ./data:/data # if run on portainer, change the(.) current directory to absolute path
-    environment:
-      WEBSOCKET_ENABLED: 'true' # Required to use websockets
-      SIGNUPS_ALLOWED: 'true'   # set to false to disable signups
---- end of file ---
-      
 # vaultwarden
 docker run -d \
 --name vaultwarden \
 -v /vw-data/:/data/ \
 -p 5555:80 \
 vaultwarden/server:latest
-
